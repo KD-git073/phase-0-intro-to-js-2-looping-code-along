07988b91e30053adfd2deb56170c4b2f8fa1d73f
Kevin Digan <kd073@hotmail.com>
2021-09-08 15:55:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+const names = ["Joe", "Caroline", "Mario"];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful gift!`);
+        }
+        return names;
+}
+
+
+writeCards(names);
---
+++ whileLoop.js
@@ -0,0 +1,8 @@
+var numb = prompt("Enter a number between 1 and 10 ");
+function countdown(numb) {
+	while (numb != 0)
+console.log(numb--)
+	
+}
+
+countdown(numb);
